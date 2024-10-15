831fb5819fe26fd2acf9642ad32766928abe3de6
Dilip kumar romi <romi.indan@gmail.com>
2024-10-16 00:03:05 +0530

    Create reactJsRouterPageRefresh_404.js

---
+++ reactJsRouterPageRefresh_404.js
@@ -0,0 +1,9 @@
+# create page : .htaccess on web hosting public folder 
+<IfModule mod_rewrite.c>
+  RewriteEngine On
+  RewriteBase /
+  RewriteRule ^index\.html$ - [L]
+  RewriteCond %{REQUEST_FILENAME} !-f
+  RewriteCond %{REQUEST_FILENAME} !-d
+  RewriteRule . /index.html [L]
+</IfModule>
